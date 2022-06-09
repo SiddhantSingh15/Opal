a966fa2d69fcbc0dffc0f9677ea1a9df21b0e644
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2022-06-09 16:53:55 +0100

    [ss5120] feat: added a basic search functionality.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../pages/Search.css";
-import { Button } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import HardwareRoundedIcon from '@mui/icons-material/HardwareRounded';
 import SearchIcon from '@material-ui/icons/Search';
+import Avatar from '@material-ui/core/Avatar';
+import {useNavigate} from 'react-router-dom';
 
 function Search() {
+
+  const [input, setInput] = useState("");
+  const history = useNavigate();
+
+  const search = (e) => {
+    e.preventDefault();
+    console.log("search!");
+
+    history("/search");
+  }
+
   return (
     <form className='search'>
-        <div className='search__input'> 
-            <SearchIcon className='search__inputIcon' />
-            <input />
+      <div className='search__input'> 
+          <SearchIcon className='search__inputIcon' />
+          <input value={input} onChange = {(e) => setInput(e.target.value)} />
+          <div class="font-icon-wrapper" onClick={search}>
             <HardwareRoundedIcon />
-        </div>
+          </div>
+      </div>
 
-        {/* <div className='search__buttons'>
-            <Button variant='outlined' type='submit'>Opal Search</Button>
-        </div> */}
     </form>
 
 
