0d7b37983e07b30b1c898d3594b110064ca21d95
fv220 <fv220@ic.ac.uk>
2022-06-13 18:55:39 +0100

    [fv220] chore: add build and test stages for actions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from "./components/NavBar";
 import Search from "./pages/Search";
 import Results from "./pages/Results";
 import useFetch from "./hooks/useFetch";
-import "typeface-open-sans"
+// import "typeface-open-sans"
 
 function App() {
   const {data: tags, isPending, error} =  useFetch("http://localhost:8000/tags");
