b19d5d429469affcadf45276cda874395dd8fa97
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2022-06-22 15:04:22 +0100

    [ss5120] feat: added pdf functionalities to doc view.

--- src/components/DocumentView.js
+++ src/components/DocumentView.js
@@ -20,7 +20,7 @@ class DocumentView extends React.Component {
 				<iframe
 					title="documentPreview"
 				 	className="document" 
-					src ="./document.html"/>
+					src ="./document.pdf"/>
 			</Modal>
 		)
 	}
