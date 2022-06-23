fd1d489a2c9b97d9849206d2fa6c7936f626454b
fv220 <fv220@ic.ac.uk>
2022-06-23 14:50:13 +0100

    fix: set right production backend uri

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  BACKEND_URI: "http://localhost:8000/api/v1",
+  BACKEND_URI: "http://35.231.0.227:8000/api/v1",
   VALID_FIELDS: ["title", "language", "type", "access", "date", "govlaw"],
   HOME_PATH: "/",
 };
