5ce228f479deb3de1f283cd2dd186eb20ddb1215
fv220 <fv220@ic.ac.uk>
2022-06-24 08:47:51 +0100

    feat: login ui

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import config from "../config";
 import { ReactComponent as OpalLogo } from "../assets/opal.svg";
 import "./NavBar.css";
+import Login from "./Login";
+import { Backdrop, Box, Stack, Typography } from "@mui/material";
+import LoginIcon from "@mui/icons-material/Login";
 
 const NavBar = () => {
   const location = useLocation();
+
+  const [login, setLogin] = useState(false);
 
   return (
     <div className="navbar">
@@ -15,6 +20,25 @@ const NavBar = () => {
           onClick={() => (window.location = config.HOME_PATH)}
         />
       )}
+      <Stack direction="row" sx={{ width: "200px" }} spacing={5}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            width: "90px",
+            cursor: "pointer",
+          }}
+          onClick={() => setLogin(true)}
+        >
+          <Typography color="white" variant="h6" sx={{ width: "100px" }}>
+            LOGIN
+          </Typography>
+          <LoginIcon sx={{ color: "white" }} />
+        </Box>
+      </Stack>
+      <Backdrop open={login} onClick={() => setLogin(false)} sx={{ zIndex: 1 }}>
+        <Login />
+      </Backdrop>
     </div>
   );
 };
